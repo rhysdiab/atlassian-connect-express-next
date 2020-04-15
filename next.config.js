174c72948ba233ea8acbf3b8fea9052e6a4cc5bd
rhys5690 <rhys5690@gmail.com>
2020-04-15 14:36:39 +1000

    added dev mode variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
  env: {
   // Reference a variable that was defined in the .env file and make it available at Build Time
   TEST_VAR: process.env.TEST_VAR,
-  SENTRY_URL: process.env.SENTRY_URL
+  SENTRY_URL: process.env.SENTRY_URL,
+  TOKEN: process.env.TOKEN,
+  HOST_BASE_URL: process.env.HOST_BASE_URL,
+  LOCAL_BASE_URL: process.env.LOCAL_BASE_URL,
+  USER_ACCOUNT_ID: process.env.USER_ACCOUNT_ID
  },
 }
--- pages/_app.js
+++ pages/_app.js
@@ -14,12 +14,32 @@ const theme = {
 
 export default class MyApp extends App {
  render() {
+
+  let { hostBaseUrl, localBaseUrl, userAccountId, token, projectKey } = this.props;
+
+  // check if dev environment and change url
+  if (!token) {
+   token = process.env.TOKEN;
+  }
+
+  if (!hostBaseUrl) {
+   hostBaseUrl = process.env.HOST_BASE_URL;
+  }
+
+  if (!localBaseUrl) {
+   localBaseUrl = process.env.LOCAL_BASE_URL;
+  }
+
+  if (!userAccountId) {
+   userAccountId = process.env.USER_ACCOUNT_ID;
+  }
+
   const { Component, pageProps, router } = this.props;
   return (
    <ErrorBoundary>
     <ThemeProvider theme={theme}>
      <MainMenu />
-     <Component {...pageProps} {...router.query} />
+     <Component {...pageProps} {...router.query} hostBaseUrl={hostBaseUrl} localBaseUrl={localBaseUrl} userAccountId={userAccountId} token={token} />
     </ThemeProvider>
    </ErrorBoundary>
   );
