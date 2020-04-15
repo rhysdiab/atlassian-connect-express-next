b68629fb75df40d926e9e469f05b6ba463b1f25b
rhys5690 <rhys5690@gmail.com>
2020-04-15 15:10:38 +1000

    cleaned up code

--- pages/hello-world.js
+++ pages/hello-world.js
@@ -2,14 +2,17 @@ import styled from 'styled-components';
 
 export default props => {
   return (
-    <div>
+    <PageContainerStyled>
       <Title>Atlassian Connect Next</Title>
       <div>{JSON.stringify(props)}</div>
-    </div>
+    </PageContainerStyled>
   );
 };
 
 const Title = styled.h1`
   font-size: 50px;
-  color: blue;
+`;
+
+const PageContainerStyled = styled.div`
+padding: 10px;
 `;
