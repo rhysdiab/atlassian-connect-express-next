62a64abb4c41715befb3866e8f0d8848e518c6a9
rhys5690 <rhys5690@gmail.com>
2020-05-03 13:24:46 +1000

    added global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from 'next/app';
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import MainMenu from '../components/MainMenu';
 import ErrorBoundary from '../components/ErrorBoundary';
 import Cancelled from '../components/Cancelled';
@@ -8,51 +8,59 @@ import Cancelled from '../components/Cancelled';
 import 'antd/dist/antd.css';
 
 const theme = {
- colors: {
-  primary: '#0070f3',
- },
+  colors: {
+    primary: '#0070f3',
+  },
 };
 
 export default class MyApp extends App {
- render() {
-  let { hostBaseUrl, localBaseUrl, userAccountId, token, projectKey, lic } =
-   this.props && this.props.router && this.props.router.query;
+  render() {
+    let { hostBaseUrl, localBaseUrl, userAccountId, token, projectKey, lic } =
+      this.props && this.props.router && this.props.router.query;
 
-  // check if dev environment and change url
-  if (!token) {
-   token = process.env.TOKEN;
+    // check if dev environment and change url
+    if (!token) {
+      token = process.env.TOKEN;
+    }
+
+    if (!userAccountId) {
+      userAccountId = process.env.USER_ACCOUNT_ID;
+    }
+
+    const { Component, pageProps, router } = this.props;
+    return (
+      <ErrorBoundary>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <MainMenu />
+          {lic === 'none' ? (
+            <Component
+              {...pageProps}
+              {...router.query}
+              hostBaseUrl={hostBaseUrl}
+              localBaseUrl={localBaseUrl}
+              userAccountId={userAccountId}
+              token={token}
+            />
+          ) : (
+              <Cancelled />
+            )}
+        </ThemeProvider>
+      </ErrorBoundary>
+    );
   }
-
-  if (!userAccountId) {
-   userAccountId = process.env.USER_ACCOUNT_ID;
-  }
-
-  const { Component, pageProps, router } = this.props;
-  return (
-   <ErrorBoundary>
-    <ThemeProvider theme={theme}>
-     <MainMenu />
-     {lic === 'active' ? (
-      <Component
-       {...pageProps}
-       {...router.query}
-       hostBaseUrl={hostBaseUrl}
-       localBaseUrl={localBaseUrl}
-       userAccountId={userAccountId}
-       token={token}
-      />
-     ) : (
-       <Cancelled />
-      )}
-    </ThemeProvider>
-   </ErrorBoundary>
-  );
- }
 }
 
 MyApp.getInitialProps = async appContext => {
- // calls page's `getInitialProps` and fills `appProps.pageProps`
- const appProps = await App.getInitialProps(appContext);
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
 
- return { ...appProps };
+  return { ...appProps };
 };
+
+const GlobalStyle = createGlobalStyle`
+
+  #__next {
+    height: 100%;
+  }
+  `;
