c894d97abd234fb5ef05390a4d2dc4024ce4dd69
rhys5690 <rhys5690@gmail.com>
2020-05-16 10:45:22 +1000

    adjusted height of loading icon contanier

--- components/Loading.js
+++ components/Loading.js
@@ -19,7 +19,7 @@ export default Loading;
 
 const SpinWrapperStyled = styled.div`
   width: 100%;
-  height: 100%;
+  height: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
